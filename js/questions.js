7a82d1fd7dc48cd5f3e0d36e95f29622c789c9c4
Quokzie <153464931+Quokzie@users.noreply.github.com>
2024-04-09 20:24:39 -0500

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "que es el soluto?",
+    answer: "la sustancia presente en menor cantidad que se disuelve en el solvente",
+    options: [
+      "la sustancia presente en menor cantidad que se disuelve en el solvente",
+      "la sustancia presente en mayor cantidad en la solución y en la cual se disuelve el solvente",
+      "la sustancia presente en igual cantidad que el solvente",
+      "la sustancia presente en x cantidad sin importar la proporcion con el solvente"
+    ]
+  },
+    {
+    numb: 2,
+    question: "oxígeno en aire es:",
+    answer: "gas en gas",
+    options: [
+      "gas en solido",
+      "gas en gas",
+      "solido en liquido",
+      "liquido en gas"
+    ]
+  },
+    {
+    numb: 3,
+    question: "azúcar disuelto en agua es:",
+    answer: "solido en liquido",
+    options: [
+      "liquido en liquido",
+      "solido en solido",
+      "gas en solido",
+      "solido en liquido"
+    ]
+  },
+    {
+    numb: 4,
+    question: "solución caracterizada por tener una pequeña cantidad de soluto disuelto en el solvente, es:",
+    answer: "solución diluida",
+    options: [
+      "solución saturada",
+      "olución concentrada",
+      "solución diluida",
+      "solución no saturada"
+    ]
+  },
+    {
+    numb: 5,
+    question: "un jugo de naranja en agua, donde la cantidad del zumo de naranja es mayor a la del agua es:",
+    answer: "solución concentrada",
+    options: [
+      "solución sobresaturada",
+      "solución no saturada",
+      "solución saturada",
+      "solución concentrada"
+    ]
+  },
+  {
+    numb: 6,
+    question: "solución que tiene una gran cantidad de soluto disuelto en el solvente, es:",
+    answer: "solución concentrada",
+    options: [
+      "solución sobresaturada",
+      "solución concentrada",
+      "solución saturada",
+      "solución no saturada"
+    ]
+  },
+  {
+    numb: 7,
+    question: "ssolución que contiene una cantidad de soluto que no ha llegado al punto maximo de disolución que tiene el solvente",
+    answer: "solución no saturada",
+    options: [
+      "solución saturada",
+      "solución no saturada",
+      "solución concentrada",
+      "solución diluida"
+    ]
+  },
+  {
+    numb: 8,
+    question: "que es el solvente?",
+    answer: "la sustancia presente en mayor cantidad en la solución y en la cual se disuelve el soluto",
+    options: [
+      "la sustancia presente en menor cantidad que se disuelve en el soluto",
+      "la sustancia presente en mayor cantidad en la solución y en la cual se disuelve el soluto",
+      "la sustancia presente en igual cantidad que el soluto",
+      "la sustancia presente en x cantidad sin importar la proporcion con el soluto"
+    ]
+  },
+  {
+    numb: 9,
+    question: "solución que tiene la cantidad maxima de soluto disuelto en el solvente, es:",
+    answer: "solución saturada",
+    options: [
+      "solución saturada",
+      "solución sobresaturada",
+      "solución no saturada",
+      "solución concentrada"
+    ]
+  },
+  {
+    numb: 10,
+    question: "solución que contienen mas soluto del que el solvente normalmente puede diluir",
+    answer: "solución sobresaturada",
+    options: [
+      "solución concentrada",
+      "solución saturada",
+      "solución no saturada",
+      "solución sobresaturada"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
